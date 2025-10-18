5e70218396c7278523be4665bffc90b8da3f6fa0
Macrowave <kraft.macrowave@gmail.com>
2025-10-18 14:00:26 -0500

    Further location renaming can be done as needed. Fixed some logic.

--- scripts/set_start.lua
+++ scripts/set_start.lua
@@ -4,7 +4,7 @@ if HAS_MAP then
     Tracker:FindObjectForCode("character_zale").Active = true
     Tracker:FindObjectForCode("character_valere").Active = true
     Tracker:FindObjectForCode("boss_wyrd").Active = true
-    Tracker:FindObjectForCode("k_hortencemap").Active = true
+    Tracker:FindObjectForCode("k_hortencemap").Active = true    
 
     -- Default options: World Access
     Tracker:FindObjectForCode("option_cavern").CurrentStage = 1
@@ -24,7 +24,7 @@ if HAS_MAP then
 
     -- Default options: Sequence Skips
     Tracker:FindObjectForCode("option_skip_prologue").CurrentStage = 1
-    Tracker:FindObjectForCode("option_lair_warp").CurrentStage = 1
+    Tracker:FindObjectForCode("option_lair_warp").CurrentStage = 0
     
     -- Default options: Logic
     Tracker:FindObjectForCode("option_logic_jocebuckle").CurrentStage = 0
